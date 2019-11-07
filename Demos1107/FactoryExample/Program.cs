f4398639d3abf3172325d25a9f4b2f9c64b06c61
Gledbury <gledbury4@gmail.com>
2019-11-07 14:58:42 -0800

    Thursdays Bogus Demos

---
+++ Demos1107/FactoryExample/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration; //req ref update
+using System.Data.Common;
+using System.Data.SqlClient;
+using static System.Console;
+
+namespace FactoryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region get config info
+            // get factory
+            DbProviderFactory factory =
+                DbProviderFactories.GetFactory(dataProvider);
+
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                if (connection == null)
+                {
+                    WriteLine("There was an issue creating the connection");
+                    return;
+                }
+                else WriteLine("-> Connection Created");
+            }
+
+            connection.ConnectionString = cnStringBuilder.ConnectionString;
+            connection.Open();
+
+            DbCommand myCommand = factory.CreateCommand();
+            if(myCommand == null) 
+                {
+                WriteLine("There was an issue creating the command");
+                return;
+            }
+            else WriteLine($"Your Command object is a " +
+                $"{myCommand.GetType().Name}");
+
+            myCommand.Connection = connection;
+            myCommand.CommandText = "Select * from Shippers";
+
+            using (DbDataReader dataReader = myCommand.ExecuteReader())
+            {
+                while (dataReader.Read())
+                {
+                    WriteLine($"-> Shipper # {dataReader["ShipperID"]} " +
+                        $"name is a {dataReader[1] } phone: {dataReader[2]}");
+                }
+            }
+
+
+
+                string dataProvider = ConfigurationManager.AppSettings["provider"];
+            WriteLine(" provider; {0}", dataProvider);
+
+            // alternate see p. 833
+            var cnStringBuilder = new SqlConnectionStringBuilder
+            {
+                InitialCatalog = "Northwind",
+                DataSource = @"(localdb)\mssqllocaldb",
+                ConnectTimeout = 30,
+                IntegratedSecurity = true
+            };
+            WriteLine($"\tBuilt Connection String: " +
+                $"{cnStringBuilder.ConnectionString}\n");
+            #endregion
+        }
+    }
+}
